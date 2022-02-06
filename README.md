38ba0572737a12d840d67b968f6d596f2d1a0c3c
Christos Pitsikas <christos@Christoss-MacBook-Pro.local>
2022-02-06 11:53:32 +0000

    Add all files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Semantic-segmentation
-Semantic Segmentation of street images for use in autonomous cars
---
+++ models.py
@@ -0,0 +1,367 @@
+from __future__ import print_function
+
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import models
+from torchvision.models.vgg import VGG
+import urllib
+import torch.nn.functional as F
+import torch.utils.model_zoo as model_zoo
+
+# FCN architectures based in https://github.com/pochih/FCN-pytorch/blob/master/python/fcn.py
+# FCN32 class modified for use of ResNet networks as backbones
+
+class FCN32s(nn.Module):
+
+    def __init__(self, pretrained_net, n_class, n_feats_backbone = 512):
+        super().__init__()
+        self.n_class = n_class
+        self.pretrained_net = pretrained_net
+        self.relu    = nn.ReLU(inplace=True)
+        self.deconv1 = nn.ConvTranspose2d(n_feats_backbone, 512, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn1     = nn.BatchNorm2d(512)
+        self.deconv2 = nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn2     = nn.BatchNorm2d(256)
+        self.deconv3 = nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn3     = nn.BatchNorm2d(128)
+        self.deconv4 = nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn4     = nn.BatchNorm2d(64)
+        self.deconv5 = nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn5     = nn.BatchNorm2d(32)
+        self.classifier = nn.Conv2d(32, n_class, kernel_size=1)
+
+
+    def forward(self, x):
+        output = self.pretrained_net(x)
+        score = self.bn1(self.relu(self.deconv1(output)))
+        score = self.bn2(self.relu(self.deconv2(score)))
+        score = self.bn3(self.relu(self.deconv3(score)))
+        score = self.bn4(self.relu(self.deconv4(score)))
+        score = self.bn5(self.relu(self.deconv5(score)))
+        score = self.classifier(score)
+        return score
+
+class FCN16s_VGG(nn.Module):
+
+    def __init__(self, pretrained_net, n_class):
+        super().__init__()
+        self.n_class = n_class
+        self.pretrained_net = pretrained_net
+        self.relu    = nn.ReLU(inplace=True)
+        self.deconv1 = nn.ConvTranspose2d(512, 512, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn1     = nn.BatchNorm2d(512)
+        self.deconv2 = nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn2     = nn.BatchNorm2d(256)
+        self.deconv3 = nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn3     = nn.BatchNorm2d(128)
+        self.deconv4 = nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn4     = nn.BatchNorm2d(64)
+        self.deconv5 = nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn5     = nn.BatchNorm2d(32)
+        self.classifier = nn.Conv2d(32, n_class, kernel_size=1)
+
+    def forward(self, x):
+        output = self.pretrained_net(x)
+        x5 = output['x5']  # size=(N, 512, x.H/32, x.W/32)
+        x4 = output['x4']  # size=(N, 512, x.H/16, x.W/16)
+
+        score = self.relu(self.deconv1(x5))               # size=(N, 512, x.H/16, x.W/16)
+        score = self.bn1(score + x4)                      # element-wise add, size=(N, 512, x.H/16, x.W/16)
+        score = self.bn2(self.relu(self.deconv2(score)))  # size=(N, 256, x.H/8, x.W/8)
+        score = self.bn3(self.relu(self.deconv3(score)))  # size=(N, 128, x.H/4, x.W/4)
+        score = self.bn4(self.relu(self.deconv4(score)))  # size=(N, 64, x.H/2, x.W/2)
+        score = self.bn5(self.relu(self.deconv5(score)))  # size=(N, 32, x.H, x.W)
+        score = self.classifier(score)                    # size=(N, n_class, x.H/1, x.W/1)
+
+        return score  # size=(N, n_class, x.H/1, x.W/1)
+
+class FCN8s_VGG(nn.Module):
+
+    def __init__(self, pretrained_net, n_class):
+        super().__init__()
+        self.n_class = n_class
+        self.pretrained_net = pretrained_net
+        self.relu    = nn.ReLU(inplace=True)
+        self.deconv1 = nn.ConvTranspose2d(512, 512, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn1     = nn.BatchNorm2d(512)
+        self.deconv2 = nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn2     = nn.BatchNorm2d(256)
+        self.deconv3 = nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn3     = nn.BatchNorm2d(128)
+        self.deconv4 = nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn4     = nn.BatchNorm2d(64)
+        self.deconv5 = nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn5     = nn.BatchNorm2d(32)
+        self.classifier = nn.Conv2d(32, n_class, kernel_size=1)
+
+    def forward(self, x):
+        output = self.pretrained_net(x)
+        x5 = output['x5']  # size=(N, 512, x.H/32, x.W/32)
+        x4 = output['x4']  # size=(N, 512, x.H/16, x.W/16)
+        x3 = output['x3']  # size=(N, 256, x.H/8,  x.W/8)
+
+        score = self.relu(self.deconv1(x5))               # size=(N, 512, x.H/16, x.W/16)
+        score = self.bn1(score + x4)                      # element-wise add, size=(N, 512, x.H/16, x.W/16)
+        score = self.relu(self.deconv2(score))            # size=(N, 256, x.H/8, x.W/8)
+        score = self.bn2(score + x3)                      # element-wise add, size=(N, 256, x.H/8, x.W/8)
+        score = self.bn3(self.relu(self.deconv3(score)))  # size=(N, 128, x.H/4, x.W/4)
+        score = self.bn4(self.relu(self.deconv4(score)))  # size=(N, 64, x.H/2, x.W/2)
+        score = self.bn5(self.relu(self.deconv5(score)))  # size=(N, 32, x.H, x.W)
+        score = self.classifier(score)                    # size=(N, n_class, x.H/1, x.W/1)
+
+        return score  # size=(N, n_class, x.H/1, x.W/1)
+
+class FCNs_VGG(nn.Module):
+
+    def __init__(self, pretrained_net, n_class):
+        super().__init__()
+        self.n_class = n_class
+        self.pretrained_net = pretrained_net
+        self.relu    = nn.ReLU(inplace=True)
+        self.deconv1 = nn.ConvTranspose2d(512, 512, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn1     = nn.BatchNorm2d(512)
+        self.deconv2 = nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn2     = nn.BatchNorm2d(256)
+        self.deconv3 = nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn3     = nn.BatchNorm2d(128)
+        self.deconv4 = nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn4     = nn.BatchNorm2d(64)
+        self.deconv5 = nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.bn5     = nn.BatchNorm2d(32)
+        self.classifier = nn.Conv2d(32, n_class, kernel_size=1)
+
+    def forward(self, x):
+        output = self.pretrained_net(x)
+        x5 = output['x5']  # size=(N, 512, x.H/32, x.W/32)
+        x4 = output['x4']  # size=(N, 512, x.H/16, x.W/16)
+        x3 = output['x3']  # size=(N, 256, x.H/8,  x.W/8)
+        x2 = output['x2']  # size=(N, 128, x.H/4,  x.W/4)
+        x1 = output['x1']  # size=(N, 64, x.H/2,  x.W/2)
+
+        score = self.bn1(self.relu(self.deconv1(x5)))     # size=(N, 512, x.H/16, x.W/16)
+        score = score + x4                                # element-wise add, size=(N, 512, x.H/16, x.W/16)
+        score = self.bn2(self.relu(self.deconv2(score)))  # size=(N, 256, x.H/8, x.W/8)
+        score = score + x3                                # element-wise add, size=(N, 256, x.H/8, x.W/8)
+        score = self.bn3(self.relu(self.deconv3(score)))  # size=(N, 128, x.H/4, x.W/4)
+        score = score + x2                                # element-wise add, size=(N, 128, x.H/4, x.W/4)
+        score = self.bn4(self.relu(self.deconv4(score)))  # size=(N, 64, x.H/2, x.W/2)
+        score = score + x1                                # element-wise add, size=(N, 64, x.H/2, x.W/2)
+        score = self.bn5(self.relu(self.deconv5(score)))  # size=(N, 32, x.H, x.W)
+        score = self.classifier(score)                    # size=(N, n_class, x.H/1, x.W/1)
+
+        return score  # size=(N, n_class, x.H/1, x.W/1)
+
+# VGG network -
+# Source: official pytorch repo https://github.com/pytorch/vision/blob/master/torchvision/models/vgg.py
+
+class VGGNet(VGG):
+    def __init__(self, pretrained=True, model='vgg16', requires_grad=True, remove_fc=True, show_params=False):
+        super().__init__(make_layers(cfg[model]))
+        self.ranges = ranges[model]
+
+        if pretrained:
+            exec("self.load_state_dict(models.%s(pretrained=True).state_dict())" % model)
+
+        if not requires_grad:
+            for param in super().parameters():
+                param.requires_grad = False
+
+        if remove_fc:  # delete redundant fully-connected layer params, can save memory
+            del self.classifier
+
+        if show_params:
+            for name, param in self.named_parameters():
+                print(name, param.size())
+
+    def forward(self, x):
+        output = {}
+
+        # get the output of each maxpooling layer (5 maxpool in VGG net)
+        for idx in range(len(self.ranges)):
+            for layer in range(self.ranges[idx][0], self.ranges[idx][1]):
+                x = self.features[layer](x)
+            output["x%d"%(idx+1)] = x
+
+        return output
+
+
+ranges = {
+    'vgg11': ((0, 3), (3, 6),  (6, 11),  (11, 16), (16, 21)),
+    'vgg13': ((0, 5), (5, 10), (10, 15), (15, 20), (20, 25)),
+    'vgg16': ((0, 5), (5, 10), (10, 17), (17, 24), (24, 31)),
+    'vgg19': ((0, 5), (5, 10), (10, 19), (19, 28), (28, 37))
+}
+
+
+cfg = {
+    'vgg16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+}
+
+def make_layers(cfg, batch_norm=False):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+
+# SegNet network based in https://github.com/delta-onera/segnet_pytorch implementation
+# Pre-trained network removed
+
+__all__ = ["segnet"]
+
+class SegNet(nn.Module):
+    @staticmethod
+    def weight_init(m):
+        if isinstance(m, nn.Linear):
+            torch.nn.init.kaiming_normal(m.weight.data)
+
+    def __init__(self, in_channels, out_channels):
+        super(SegNet, self).__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+
+        self.pool = nn.MaxPool2d(2, return_indices=True)
+        self.unpool = nn.MaxUnpool2d(2)
+
+        self.conv1_1 = nn.Conv2d(in_channels, 64, 3, padding=1)
+        self.conv1_1_bn = nn.BatchNorm2d(64)
+        self.conv1_2 = nn.Conv2d(64, 64, 3, padding=1)
+        self.conv1_2_bn = nn.BatchNorm2d(64)
+
+        self.conv2_1 = nn.Conv2d(64, 128, 3, padding=1)
+        self.conv2_1_bn = nn.BatchNorm2d(128)
+        self.conv2_2 = nn.Conv2d(128, 128, 3, padding=1)
+        self.conv2_2_bn = nn.BatchNorm2d(128)
+
+        self.conv3_1 = nn.Conv2d(128, 256, 3, padding=1)
+        self.conv3_1_bn = nn.BatchNorm2d(256)
+        self.conv3_2 = nn.Conv2d(256, 256, 3, padding=1)
+        self.conv3_2_bn = nn.BatchNorm2d(256)
+        self.conv3_3 = nn.Conv2d(256, 256, 3, padding=1)
+        self.conv3_3_bn = nn.BatchNorm2d(256)
+
+        self.conv4_1 = nn.Conv2d(256, 512, 3, padding=1)
+        self.conv4_1_bn = nn.BatchNorm2d(512)
+        self.conv4_2 = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv4_2_bn = nn.BatchNorm2d(512)
+        self.conv4_3 = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv4_3_bn = nn.BatchNorm2d(512)
+
+        self.conv5_1 = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv5_1_bn = nn.BatchNorm2d(512)
+        self.conv5_2 = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv5_2_bn = nn.BatchNorm2d(512)
+        self.conv5_3 = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv5_3_bn = nn.BatchNorm2d(512)
+
+        self.conv5_3_D = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv5_3_D_bn = nn.BatchNorm2d(512)
+        self.conv5_2_D = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv5_2_D_bn = nn.BatchNorm2d(512)
+        self.conv5_1_D = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv5_1_D_bn = nn.BatchNorm2d(512)
+
+        self.conv4_3_D = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv4_3_D_bn = nn.BatchNorm2d(512)
+        self.conv4_2_D = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv4_2_D_bn = nn.BatchNorm2d(512)
+        self.conv4_1_D = nn.Conv2d(512, 256, 3, padding=1)
+        self.conv4_1_D_bn = nn.BatchNorm2d(256)
+
+        self.conv3_3_D = nn.Conv2d(256, 256, 3, padding=1)
+        self.conv3_3_D_bn = nn.BatchNorm2d(256)
+        self.conv3_2_D = nn.Conv2d(256, 256, 3, padding=1)
+        self.conv3_2_D_bn = nn.BatchNorm2d(256)
+        self.conv3_1_D = nn.Conv2d(256, 128, 3, padding=1)
+        self.conv3_1_D_bn = nn.BatchNorm2d(128)
+
+        self.conv2_2_D = nn.Conv2d(128, 128, 3, padding=1)
+        self.conv2_2_D_bn = nn.BatchNorm2d(128)
+        self.conv2_1_D = nn.Conv2d(128, 64, 3, padding=1)
+        self.conv2_1_D_bn = nn.BatchNorm2d(64)
+
+        self.conv1_2_D = nn.Conv2d(64, 64, 3, padding=1)
+        self.conv1_2_D_bn = nn.BatchNorm2d(64)
+        self.conv1_1_D = nn.Conv2d(64, out_channels, 3, padding=1)
+
+        self.apply(self.weight_init)
+
+    def forward(self, x):
+        # Encoder block 1
+        x = F.relu(self.conv1_1_bn(self.conv1_1(x)))
+        x1 = F.relu(self.conv1_2_bn(self.conv1_2(x)))
+        size1 = x.size()
+        x, mask1 = self.pool(x1)
+
+        # Encoder block 2
+        x = F.relu(self.conv2_1_bn(self.conv2_1(x)))
+        x2 = F.relu(self.conv2_2_bn(self.conv2_2(x)))
+        size2 = x.size()
+        x, mask2 = self.pool(x2)
+
+        # Encoder block 3
+        x = F.relu(self.conv3_1_bn(self.conv3_1(x)))
+        x = F.relu(self.conv3_2_bn(self.conv3_2(x)))
+        x3 = F.relu(self.conv3_3_bn(self.conv3_3(x)))
+        size3 = x.size()
+        x, mask3 = self.pool(x3)
+
+        # Encoder block 4
+        x = F.relu(self.conv4_1_bn(self.conv4_1(x)))
+        x = F.relu(self.conv4_2_bn(self.conv4_2(x)))
+        x4 = F.relu(self.conv4_3_bn(self.conv4_3(x)))
+        size4 = x.size()
+        x, mask4 = self.pool(x4)
+
+        # Encoder block 5
+        x = F.relu(self.conv5_1_bn(self.conv5_1(x)))
+        x = F.relu(self.conv5_2_bn(self.conv5_2(x)))
+        x = F.relu(self.conv5_3_bn(self.conv5_3(x)))
+        size5 = x.size()
+        x, mask5 = self.pool(x)
+
+        # Decoder block 5
+        x = self.unpool(x, mask5, output_size = size5)
+        x = F.relu(self.conv5_3_D_bn(self.conv5_3_D(x)))
+        x = F.relu(self.conv5_2_D_bn(self.conv5_2_D(x)))
+        x = F.relu(self.conv5_1_D_bn(self.conv5_1_D(x)))
+
+        # Decoder block 4
+        x = self.unpool(x, mask4, output_size = size4)
+        x = F.relu(self.conv4_3_D_bn(self.conv4_3_D(x)))
+        x = F.relu(self.conv4_2_D_bn(self.conv4_2_D(x)))
+        x = F.relu(self.conv4_1_D_bn(self.conv4_1_D(x)))
+
+        # Decoder block 3
+        x = self.unpool(x, mask3, output_size = size3)
+        x = F.relu(self.conv3_3_D_bn(self.conv3_3_D(x)))
+        x = F.relu(self.conv3_2_D_bn(self.conv3_2_D(x)))
+        x = F.relu(self.conv3_1_D_bn(self.conv3_1_D(x)))
+
+        # Decoder block 2
+        x = self.unpool(x, mask2, output_size = size2)
+        x = F.relu(self.conv2_2_D_bn(self.conv2_2_D(x)))
+        x = F.relu(self.conv2_1_D_bn(self.conv2_1_D(x)))
+
+        # Decoder block 1
+        x = self.unpool(x, mask1, output_size = size1)
+        x = F.relu(self.conv1_2_D_bn(self.conv1_2_D(x)))
+        x = self.conv1_1_D(x)
+        return x
+
+
+def segnet(in_channels, out_channels, pretrained=False, **kwargs):
+    model = SegNet(in_channels, out_channels)
+    return model
